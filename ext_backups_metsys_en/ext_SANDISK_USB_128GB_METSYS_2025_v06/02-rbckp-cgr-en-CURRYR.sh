d005dd64b2c12055e4870818cce4d516dbf39ae1
r-gregor <gredelonghi@gmail.com>
2025-04-30 12:49:08 +0200

    update 20250430_1249_en

---
+++ ext_backups_metsys_en/ext_SANDISK_USB_128GB_METSYS_2025_v06/02-rbckp-cgr-en-CURRYR.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+
+# 02-rbckp-cgr-en-CURRYR.sh
+# 20250430
+# *********************************************************************************************************
+
+if [ $CURRENT_YEAR_ENV -ne $(date +%Y) ]; then
+	curryr=$CURRENT_YEAR_ENV
+else
+	curryr=$(date +%Y)
+fi
+
+function tms() {
+    echo -n "[ $(date +%Y%m%d_%H%M%S) ] "
+}
+
+crtn=3
+function crtc() {
+    for ((i=1; i<=$1; i++)); do
+        echo -n "-"
+    done
+    echo
+}
+
+gr_CURRDATE="$(date +%Y%m%d)"
+gr_DANES="$(date +%Y%m%d)"
+gr_CURRT="$(date +%Y%m%d_%H%M%S)"
+
+gr_CURDRV=$(cd $(dirname ${BASH_SOURCE[0]}) && cd ../ && pwd)
+gr_0="$(realpath $0)"
+
+gr_RSO='rsync -rltD --update --info=name0,stats2,progress0 --modify-window=2'           # RUN backup ptions
+gr_FILT='/usr/bin/grep "files\|total"'
+
+export gr_DRIVE=${gr_CURDRV}
+export gr_BEKAPD=${gr_DRIVE}/bekaps_en
+export gr_DNBCKP=${gr_DRIVE}/en_METSYS/DONOTBACKUP.txt
+export gr_CNT=${gr_DRIVE}/en_METSYS/tcount
+
+# RUN ...
+dT1=$(date +"%s")
+(tms; echo "Starting ${gr_0} ... ")
+
+tms; echo "Current Drive is: [ ${gr_CURDRV} ]."
+
+if [ ! -e ${gr_BEKAPD} ]; then
+    (tms; echo "Wrog DRIVE letter or there is no ${gr_BEKAPD}. Aborting ...")
+	exit 1
+fi
+
+if [ ! -e ${gr_DNBCKP} ]; then
+    (tms; echo "Wrog DRIVE letter or there is no ${gr_DNBCKP}. Aborting ...")
+	exit 1
+fi
+
+tms; echo "${gr_BEKAPD} and ${gr_DNBCKP} exist. It is safe to continue ..."
+
+cd ${gr_BEKAPD}
+tms; echo "Current directory is ${PWD} ..."
+
+
+# START
+(tms; echo "Backing up /c/Users/gregor.redelonghi/${curryr} to ${gr_BEKAPD} ... ")
+(tms; echo "Syncing ...  ")
+${gr_RSO} --delete /c/Users/gregor.redelonghi/${curryr}/ ${gr_BEKAPD}/CURRYR_cugr_en/ | eval ${gr_FILT} | while read line; do (tms; echo $line); done
+(tms; crtc $crtn)
+
+
+dT2=$(date +"%s")
+dlt=$((dT2-dT1))
+time=$(($(cat ${gr_CNT})+${dlt}))
+echo $time > ${gr_CNT}
+(tms; echo "${gr_0} done in $dlt seconds!")
+crtc $crtn
+
+exit
+
